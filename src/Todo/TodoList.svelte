9507dc68fe56243c06d07be0663d3fa3bdd80c20
Johannes Leussler <johannes.leussler@gmail.com>
2022-01-22 13:09:15 +0100

    Fixed broken application and updated gitignore

--- src/Todo/TodoList.svelte
+++ src/Todo/TodoList.svelte
@@ -1,51 +1,48 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import TodoItem from "./TodoItem.svelte";
+	import { onMount } from "svelte";
+	import TodoItem from "./TodoItem.svelte";
 
-    let todos: string[] = [];
-    let todoName: string = "";
+	let todos: string[] = [];
+	let todoName: string = "";
 
-    onMount(() => {
-        const existingTodos = localStorage.getItem("todos");
-        todos = JSON.parse(existingTodos) || [];
-    });
+	onMount(() => {
+		const existingTodos = localStorage.getItem("todos");
+		todos = JSON.parse(existingTodos) || [];
+	});
 
+	const addTodo = () => {
+		if (todoName) {
+			todos = [...todos, todoName];
+			localStorage.setItem("todos", JSON.stringify(todos));
+			todoName = "";
+		}
+	};
 
-    asd
-
-    const addTodo = () => {
-        if (todoName) {
-            todos = [...todos, todoName];
-            localStorage.setItem("todos", JSON.stringify(todos));
-            todoName = "";
-        }
-    };
-
-    const removeTodo = (todo: string) => {
-        todos = todos.filter((item) => item != todo);
-        localStorage.setItem("todos", JSON.stringify(todos));
-    };
+	const removeTodo = (todo: string) => {
+		todos = todos.filter((item) => item != todo);
+		localStorage.setItem("todos", JSON.stringify(todos));
+	};
 </script>
 
 <form on:submit|preventDefault={addTodo}>
-    <input bind:value={todoName} placeholder="Insert todo" />
-    <input type="submit" value="Add" />
+	<input bind:value={todoName} placeholder="Insert todo" />
+	<input type="submit" value="Add" />
 </form>
 
 <ul>
-    {#each todos as todo}
-        <TodoItem name={todo} handleRemove={removeTodo} />
-    {/each}
+	{#each todos as todo}
+		<TodoItem name={todo} handleRemove={removeTodo} />
+	{/each}
 </ul>
 
 <style>
-    ul {
-        list-style: none;
-    }
-    input[type="submit"] {
-        background-color: darkgreen;
-        cursor: pointer;
-        color: white;
-        border-radius: 0.4rem;
-    }
+	ul {
+		list-style: none;
+	}
+	input[type="submit"] {
+		background-color: darkgreen;
+		cursor: pointer;
+		color: white;
+		border-radius: 0.4rem;
+	}
 </style>
